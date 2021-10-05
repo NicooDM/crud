6f430e509f2db19b1f96ca1be3c05f7c4f891dca
Nicolas Di Marco <dimarconicolas5@gmail.com>
2021-10-05 18:35:07 -0300

    se valido el formulario

--- js/validaciones.js
+++ js/validaciones.js
@@ -3,9 +3,23 @@ const validarCampoRequerido = (input) => {
   if (input.value.trim() != "") {
     console.log("el dato es correcto");
     input.className="form-control is-valid"
+    return true
   } else {
     console.log("corregir el dato");
     input.className="form-control is-invalid"
+    return false
+  }
+};
+const validarCodigo = (input) => {
+  //preguntar si un input tiene un cadena de texto vacia tmb se lo puede hacer con .length
+  if (input.value.trim() != "" && input.value.trim().length>=3) {
+    console.log("el dato es correcto");
+    input.className="form-control is-valid"
+    return true
+  } else {
+    console.log("corregir el dato");
+    input.className="form-control is-invalid"
+    return false
   }
 };
 const validarNros=(input)=>{
@@ -13,17 +27,51 @@ const validarNros=(input)=>{
     let patron=/^[0-9]{1,3}$/
     if(patron.test(input.value)){
         input.className="form-control is-valid"
+        return true
     }
     else{
         input.className="form-control is-invalid"
+        return false
     }
 }
+const validarURL=(input)=>{
+  let patron = /^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/
+  if(patron.test(input.value.trim()!=''&&input.value.trim())){
+    input.className='form-control is-valid'
+    return true
+  }
+  else{
+    input.className='form-control is-invalid'
+    return false
+  }
+}
+//se puede poner event o e :)
+const validarGeneral=(e)=>{
+  //preventdefault previene q recarge la pagina
+  //console.log('desde la funcion validar general')
+  e.preventDefault()
+  //if(true),else(false)
+  if(validarCodigo(codigo)&&validarURL(url)&&validarNros(cantidad)&&validarCampoRequerido(producto)){
+    console.log('aca se debe cargar el producto')
 
+  }else{
+console.log('error,')
+  }
+
+}
 //traer los inputs(o textarea) que quiera validar
 let codigo= document.querySelector('#codigo')
 let cantidad = document.querySelector('#cantidad')
-
+let url = document.querySelector('#url')
+let producto = document.querySelector('#producto')
+let descripcion = document.querySelector('#descripcion')
+let formulario = document.querySelector('#formProducto')
 //le agregamos el evento que queremos
-codigo.addEventListener('blur',()=>{validarCampoRequerido(codigo)})
+codigo.addEventListener('blur',()=>{validarCodigo(codigo)})
 cantidad.addEventListener('blur',()=>{validarNros(cantidad)})
+url.addEventListener('blur',()=>{validarURL(url)})
+producto.addEventListener('blur',()=>{validarCampoRequerido(producto)})
+descripcion.addEventListener('blur',()=>{validarCampoRequerido(descripcion)})
+formulario.addEventListener('submit',validarGeneral)
+
 
